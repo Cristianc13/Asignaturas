3eb09e8c2d152de807576cf5b39bafa49f2838cd
Cristian Canales <55901424+Cristianc13@users.noreply.github.com>
2019-12-02 17:51:59 -0600

    Creando modelo estudiante

--- AFK/AFK/Models/Estudiante.cs
+++ AFK/AFK/Models/Estudiante.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class Class1
+namespace AFK.Models
 {
-    public Class1()
+    public class Estudiante
     {
+        [Key]
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+
+
+        //Llave foranea
+        [ForeignKey("Asignatura")]
+        public int? IDAsignatura { get; set; }
+
+        [ForeignKey("IDAsignatura")]
+        public virtual Asignatura Asignatura { get; set; }
 
     }
 }
-	
