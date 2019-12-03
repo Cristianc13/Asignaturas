983e13a375eed5e4eb7f5647be5f1ab5e41676e1
Alberto Jose Blandon Moreno <ialbertbla98@gmail.com>
2019-12-02 18:06:47 -0600

    Modelo Asignaturas Terminado

---
+++ AFK/AFK/Models/Asignatura.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AFK.Models
+{
+    public class Asignatura
+    {
+        [Key]
+        public int IDAsignatura { get; set; }
+        public string NombreAsignatura { get; set; }
+    }
+}
